457926f0a1880f146f0912e7614308a7c48ad603
Mktm <mody.kane60100@gmail.com>
2024-09-11 09:23:43 +0200

    add portfolio v1

---
+++ config.js
@@ -0,0 +1,10 @@
+let config = {
+  emailjs: {
+    publicKey: 'Q76zvI2fXZ23JChxr',
+    serviceID: "default_service",
+    templateID: "template_lo7f71y",
+    user_id: "Q76zvI2fXZ23JChxr"
+  }
+};
+
+export default config;
---
+++ info.js
@@ -0,0 +1,478 @@
+let info = {
+  name: "Mody KANE",
+  logo_name: "Mody",
+  config: {
+    use_cookies: true,
+    navbar: {
+      blur: false
+    }
+  },
+  description:
+    "Hello",
+  links: {
+    linkedin: "https://www.linkedin.com/in/mody-kane-0390bb205",
+    github: "https://github.com/mktmo",
+    resume: "https://cvdesignr.com/p/5c6ea5c8e5191"
+  },
+  education: [
+    {
+      name: "Estiam Paris ",
+      place: "Paris, 75020",
+      date: "octobre, 2022 - present",
+      degree: "master développeur fullstack",
+
+      description:
+        "",
+      skills: [
+        "Animation 3D (Unreal Engine 5)",
+        "AWS",
+        "Back-end developing (GO, Python)",
+        "Blockchain",
+        "Database administration",
+        "E-industries", 
+        "Front-end developing (React JS)",
+        "Mobile application (React Native)",
+        "Process automatisation",
+        "Quantique computing",
+        "Software Engineering",
+        "Systeme administration",
+        "UI/UX design",
+        "Web programming",
+      ]
+    },
+
+    {
+      name: "Estiam Paris",
+      place: "Paris, 75020",
+      date: "octobre, 2021 - septembre 2022",
+      degree: "Bachelor développeur fullstack",
+
+      description:
+        "",
+      skills: [
+        "Agile method",
+        "Azure",
+        "Back-end developing (Node JS, Python)",
+        "Buisness intelligence",
+        "Cloud computing",
+        "Database administration",
+        "Front-end developing (React JS)",
+        "Linux virtualisation",
+        "Software Engineering",
+        "UI/UX design",
+        "Web programming",
+      ]
+    },
+
+    {
+      name: "Greta, Compiègne",
+      place: "Compiègne, 60200",
+      date: "décembre, 2019 - février 2021",
+      degree: "Licence Concepteur Développeur d'Application (CDA)",
+
+      description:
+        "",
+      skills: [
+        "Analyse et programmation",
+        "Conception et développement de la partie front-end et back-end d'interfaces web",
+        "Maquettage d'application mobile et de site web",
+        "Mise en place et gestion de base de donnée",
+        "Tests et déploiement d'application",
+      ]
+    },
+
+    {
+      name: "Lycée Jules Uhry",
+      place: "Creil, 60100",
+      date: "septembre, 2013 - juin 2016",
+      degree: "Bac Science et Technologie du Management et de la Gestion",
+
+      description:
+        "",
+      skills: [
+        "Comptabilité",
+        "Droit",
+        "Economie",
+        "Management",
+      ]
+    },
+  ],
+  experience: [
+    {
+      name: "Direction Générale de la Gendarmerie Nationale",
+      place: "Issy les Moulineaux, 92130",
+      date: "novembre 2021 - Actuellement",
+      position: "Développeur Fullstack Javascript",
+      description:
+        "",
+      skills: [ "Administration Linux", "Angular", "Docker", "Javascript", "Nest JS", "Node JS", "Postgre SQL", "Quasar", "Typescript", "Vue JS", ]
+    },
+    {
+      name: "AKOG Services",
+      place: "Noisiel, 77186",
+      date: "Août 2020 - septembre, 2020",
+      position: "Développeur web",
+      description:
+        "",
+      skills: ["API REST", "Bootstrap", "CSS3", "es6", "HTML5", "JavaSript", "JQuery", "Json",  "TypeSript",  "VB", ".NET", ]
+    },
+  ],
+  skills: [
+    {
+      title: "Language",
+      info: [
+        "C#", "Java", "Javascript", "Python", "SQL", "TypeScript",],
+      icon: "fa fa-code"
+    },
+    {
+      title: "Methodology",
+      info: [
+        "AGILE", "KANBAN", "MERISE", "SCRUM", "DEVOPS", "UML"
+       ],
+      icon: "fa fa-tasks"
+    },
+    {
+      title: "Security",
+      info: [
+        "OAUTH", "SSO", "SSL"
+       ],
+      icon: "fa fa-lock"
+    },
+
+    {
+      title: "Systems",
+      info: [
+         "DEBIAN", "LINUX", "UNIX", "WINDOWS"
+       ],
+      icon: "fa fa-desktop"
+    },
+    {
+      title: "SGBD",
+      info: [
+        "MONGODB", "NoSQL", "POSTGRESQL","SQL", "SERVER", "MARIADB", "MYSQL", "SQLITE"
+       ],
+      icon: "fa fa-database"
+    },
+    {
+      title: "web technologies",
+      info: ["Angular", "ASP .NET", "Bootstrap3", "CSS3", "Es6", "HTML5", "Nest JS", "Node JS", "Quasar", "React", "Vue JS", ],
+      icon: "fas fa-laptop-code"
+    },
+    {
+      title: "UX/UI Design",
+      info: ["Figma", "Gimp", "Illustrator", "Photoshop", ],
+      icon: "fa fa-pencil-square-o"
+    },
+  ],
+  portfolio: [
+    
+    {
+      name: "CORG",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/CORG/Prediction.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Cycle.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Dashboard.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Planning.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Effectif.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Departement.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Droit.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Menus.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Role.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Utilisateur.png")
+        }
+      ],
+      technologies: ["Vue JS (Quasar)", "Nest JS", "Postgre SQL"],
+      category: "Web & mobile App",
+      github: "https://github.com/MKtmo",
+      date: "2023",
+      description:
+        "Application qui permet la gestion des effectifs du personnel qui est chargé de répondre au besoin lors des appels."
+    }, 
+    
+/*
+    {
+      name: "Gest Students",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/CORG/Prediction.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Cycle.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Dashboard.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Planning.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Effectif.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Departement.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Droit.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Menus.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Role.png")
+        },
+        {
+          img: require("./src/assets/portfolio/CORG/Utilisateur.png")
+        }
+      ],
+      technologies: ["Java"],
+      category: "Web",
+      github: "https://github.com/MKtmo",
+      date: "2022",
+      description:
+        "Application qui permet la gestion des étudiants."
+    }, */
+
+    {
+      name: "KYNOS",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/Kynos/Gestion_user.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Kynos/Ajout_user.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Kynos/Modifier_user.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Kynos/Carto.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Kynos/Info_bulle_carto.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Kynos/Filtre_carto.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Kynos/Planning.png")
+        }
+      ],
+      technologies: ["Vue JS (Quasar)", "Nest JS", "Postgre SQL"],
+      category: "Web & mobile App",
+      github: "https://github.com/MKtmo",
+      date: "2022",
+      description:
+        "Application qui permet au personnel Cynophile de pouvoir géolocaliser les différents maître-chien avec différents statuts. D'établir un planning avec les différents maître-chien en fonction de leur disponibilité. Lister les utilisateurs en fonction du profil connecté avec les filtres, recherches et CRUD. Elle est optimisée pour mobile."
+    }, 
+
+    {
+      name: "RNI (Réponse de Négociation Immédiate)",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/RNI/Gestion_nego.png")
+        },
+        {
+          img: require("./src/assets/portfolio/RNI/Ajout_nego.png")
+        },
+        {
+          img: require("./src/assets/portfolio/RNI/Modif_nego.png")
+        },
+        {
+          img: require("./src/assets/portfolio/RNI/Carto.png")
+        },
+        {
+          img: require("./src/assets/portfolio/RNI/Info_bulle_carto.png")
+        },
+        {
+          img: require("./src/assets/portfolio/RNI/Ajout_crise_choix.png")
+        },
+        {
+          img: require("./src/assets/portfolio/RNI/Ajout_crise .png")
+        },
+        {
+          img: require("./src/assets/portfolio/RNI/Filtre.png")
+        },
+        {
+          img: require("./src/assets/portfolio/RNI/Planning.png")
+        },
+      ],
+      technologies: ["Angular", "Node JS", "Postgre SQL"],
+      category: "Web & mobile App",
+      github: "https://github.com/MKtmo",
+      date: " 2021",
+      description:
+        "Application qui permet au personnel du GIGN de pouvoir géolocaliser les différents négociateurs avec différents statuts. D'établir un planning avec les différents négociateurs en fonction de leur disponibilité. Lister les utilisateurs en fonction du profil connecté avec les filtres, recherches et CRUD. Elle est optimisée pour mobile."
+    },
+
+    {
+      name: "Gestion App",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/GestionApp/Gestion_app.png")
+        },
+        {
+          img: require("./src/assets/portfolio/GestionApp/Add.png")
+        },
+        {
+          img: require("./src/assets/portfolio/GestionApp/Update.png")
+        },
+        {
+          img: require("./src/assets/portfolio/GestionApp/Delete.png")
+        },
+        {
+          img: require("./src/assets/portfolio/GestionApp/Demande.png")
+        },
+        {
+          img: require("./src/assets/portfolio/GestionApp/Menu.png")
+        },
+        {
+          img: require("./src/assets/portfolio/GestionApp/Profil.png")
+        },
+        {
+          img: require("./src/assets/portfolio/GestionApp/Vote.png")
+        }
+      ],
+      technologies: ["Vue JS (Quasar)", "Nest JS", "Postgre SQL"],
+      category: "Web & mobile App",
+      github: "https://github.com/MKtmo",
+      date: "2024", 
+      description:
+        "Application qui permet au personnel Gendarmerie de donner leur avis sur des applications existantes. Ils peuvent également proposer d'éventuelles d'évolutions qui seront ensuite votées."
+    }, 
+
+    {
+      name: "Labyrinthe Shinmobil",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/Lab_Shinmobil/Accueil.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Lab_Shinmobil/Menu_pause.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Lab_Shinmobil/Plan_lab.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Lab_Shinmobil/Lab_classique.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Lab_Shinmobil/Lab_dans_le_noir.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Lab_Shinmobil/Lab_téléportation.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Lab_Shinmobil/Escape.png")
+        }
+      ],
+      technologies: ["Unreal Engine 5"],
+      category: "Animation 3D",
+      github: "https://github.com/MKtmo",
+      date: "2024",
+      description:
+        "Jeu réalisé sur Unreal Engine 5. Il dispose de différents défis à réaliser (empilement, labyrinthe)."
+    }, 
+
+    {
+      name: "Pokédex ShinMo",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/Pokedex/Pokedex.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Pokedex/Pokedex_filtre_fire.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Pokedex/Pokedex_filtre_fire name.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Pokedex/Pokedex_filtre_fire_name_search.png")
+        },
+      ],
+      technologies: ["React", "Node JS"],
+      category: "Web & mobile App",
+      github: "https://github.com/MKtmo/Pokedex-ShinMo.git",
+      date: " 2022",
+      description:
+        "Application dans laquelle on retrouve la liste des différents pokémon. Ils sont affichés sous forme de cartes avec un sytème de filtre et de recherche."
+    },
+
+    {
+      name: "Driving",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/Driving/Accueil.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Driving/Connexion.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Driving/Utilisateur.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Driving/Trajet.jpg")
+        },
+        {
+          img: require("./src/assets/portfolio/Driving/Choix.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Driving/Recap.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Driving/Map.png")
+        },
+        {
+          img: require("./src/assets/portfolio/Driving/Notif.png")
+        },
+      ],
+      technologies: ["Android", 'Firebase'],
+      category: "mobile App",
+      github: "https://github.com/MKtmo/",
+      date: " 2022",
+      description:
+        "Application de vtc qui a pour but de faire disparaître les prix douteux grâce à une facturation détailler pour les clients."
+    },
+
+    {
+      name: "M'Hotel",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/M_Hotel/Hotel_list.png")
+        },
+        {
+          img: require("./src/assets/portfolio/M_Hotel/New_badge_hotel.png")
+        },
+        {
+          img: require("./src/assets/portfolio/M_Hotel/Search.png")
+        },
+      ],
+      technologies: ["Angular", "Node JS", "Postgre SQL"],
+      category: "Web & mobile App",
+      github: "https://github.com/MKtmo/FirsAppAngular.git",
+      date: " 2021",
+      description:
+        "Application qui permet la gestion et l'affichage dynamique d'hôtels dans le monde. Elle dispose d'un filtre de recherche et d'une gestion d'affichage avec un badge de nouveauté."
+    },
+  ],
+};
+
+export default info;
